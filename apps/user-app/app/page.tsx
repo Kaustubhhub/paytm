fb246119f6b86ba02134cb002243a77a48c5576c
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-07-08 00:32:39 +0530

    bootstrap-application

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="text-blue-400">
+      hi from page
+    </div>
+  );
+}
